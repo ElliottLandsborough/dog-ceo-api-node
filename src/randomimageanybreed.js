9b2a76b4a8e55ca8dc211ebc3448e743f0c1d6a2
Elliott Landsborough <elliott.landsborough@gmail.com>
2018-04-25 12:00:10 +0100

    Add empty endpoint for now

--- src/randomimageanybreed.js
+++ src/randomimageanybreed.js
@@ -9,6 +9,16 @@ module.exports.randomimageanybreed = (event, context, callback) => {
     region : 'eu-west-1'
   });
 
+  var path = event.path;
+  var cleanPath = path.replace(/^\/|\/$/g, '');
+  var split = cleanPath.split('/');
+  var amount = parseInt(split[3], 10);
+
+  // make sure we have an int of 1 or more
+  if (!amount) {
+    amount = 1;
+  }
+
   var params = {
     Bucket: 'dog-ceo-stanford-files',
     Delimiter: "/",
@@ -29,35 +39,50 @@ module.exports.randomimageanybreed = (event, context, callback) => {
     }
 
     var s3BreedList = result.CommonPrefixes;
-    var s3RandomBreed = s3BreedList[Math.floor(Math.random()*s3BreedList.length)];
-    var breed = s3RandomBreed.Prefix;
-    var cleanBreed = breed.replace(/[^a-z\-]/g,'');
 
-    var params = {
-      Bucket: 'dog-ceo-stanford-files',
-      Delimiter: '',
-      Prefix: cleanBreed + '/',
-      MaxKeys: 1000
-    };
+    if (amount == 1) {
+      var s3RandomBreed = s3BreedList[Math.floor(Math.random()*s3BreedList.length)];
+      var breed = s3RandomBreed.Prefix;
+      var cleanBreed = breed.replace(/[^a-z\-]/g,'');
 
-    s3.listObjectsV2(params, function (error, result) {
+      var params = {
+        Bucket: 'dog-ceo-stanford-files',
+        Delimiter: '',
+        Prefix: cleanBreed + '/',
+        MaxKeys: 1000
+      };
 
-      var s3BreedObjects = result.Contents;
-      var s3Object = s3BreedObjects[Math.floor(Math.random()*s3BreedObjects.length)];
-      var s3File = 'https://images.dog.ceo/breeds/' + s3Object.Key;
+      s3.listObjectsV2(params, function (error, result) {
 
-      var responseObject = {status: 'success', message: s3File};
+        var s3BreedObjects = result.Contents;
+        var s3Object = s3BreedObjects[Math.floor(Math.random()*s3BreedObjects.length)];
+        var s3File = 'https://images.dog.ceo/breeds/' + s3Object.Key;
 
+        var responseObject = {status: 'success', message: s3File};
+
+        // create a response
+        var response = {
+          statusCode: 200,
+          headers: {'cache-control': 'private, no-cache'},
+          body: JSON.stringify(responseObject)
+        };
+
+        callback(null, response);
+      });
+    }
+
+    // might want to add something in here if we want to have recursive s3 queries?
+    if (amount > 1) {
       // create a response
       var response = {
         statusCode: 200,
         headers: {'cache-control': 'private, no-cache'},
-        body: JSON.stringify(responseObject)
+        body: JSON.stringify(['This should not ever be called'])
       };
 
       callback(null, response);
+    }
 
-    });
   });
 
 };
